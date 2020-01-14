6f28709991f5d42640f71482d796151a67e1a410
ymi05 <44197563+ymi05@users.noreply.github.com>
2020-01-14 03:09:08 -0800

    Add files via upload

---
+++ Pages/events.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Landsmannschaft Beyrouth</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../CSS/events.css">
+    <link href="https://fonts.googleapis.com/css?family=Germania+One&display=swap" rel="stylesheet">
+  </head>
+<body>
+        <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="../index.html">Landsmannschaft Beyrouth</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="../index.html">Home <span class="sr-only">(current)</span></a>
+                <!-- <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a> -->
+              </div>
+            </div>
+        </nav>
+      <div class="Events-area">
+        <div class="text-part">
+          <h1>Events</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, officia? Voluptatem in optio beatae. Officiis impedit earum cumque quis iste maxime, soluta dolorum fugiat, quisquam omnis quos nostrum, corrupti similique! Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quas nostrum blanditiis odio in, nemo rerum non obcaecati vitae totam similique veritatis facere! Placeat quo tempore eum fugit, distinctio vel.</p>
+        </div>
+        <hr>
+        
+      </div>
+      <div class="listing">
+          <h1>Orientation Day</h1>
+          <ul class="tabular-data">
+            <li><span>Start</span><span class="info">Saturday, February 15, 2020 @ 12:00 AM</span></li>
+            <li><span>End</span><span class="info">	Saturday, February 15, 2020 @ 2:00 PM</span></li>
+            <li><span>Location</span><span class="info"></span></li>
+          </ul>
+      </div>
+      <script src="../JS/nav.js"></script>
+    </body>
+</html>
