c95b96f541d8577936ee308ede0b7492889c7b8d
ymi05 <44197563+ymi05@users.noreply.github.com>
2020-01-14 01:44:25 -0800

    Add files via upload

--- JS/nav.js
+++ JS/nav.js
@@ -1,17 +1,34 @@
 
+var currentlyActive;
 var sections = document.querySelectorAll(".navbar-nav a");
-for(let i=0; i<sections.length;i++){
-    sections[i].addEventListener("click",()=>{
-        let width;
-        if(window.outerHeight){
-            width = window.outerWidth;
-        }
-        else {
-            width = document.body.clientWidth;
-        }
-        if(width<=768){
-            document.querySelector("button").click();
+var activeClass_Elements = document.querySelectorAll(".active");
 
-        }
-    });
+for(let i=0; i<sections.length;i++){
+    sections[i].addEventListener("click",clicked);
+    if(sections[i].classList.contains("active")){
+        currentlyActive = sections[i];
+    }
+}
+
+
+
+function clicked(){
+    let width;
+    let element = this;
+    console.log(this.innerHTML);
+    console.log(activeClass_Elements);
+    if(window.outerHeight){
+        width = window.outerWidth;
+    }
+    else {
+        width = document.body.clientWidth;
+    }
+    if(width<=768){
+        document.querySelector("button").click();
+    }
+    this.classList.add("active");
+    currentlyActive.classList.remove("active");
+    currentlyActive = this;
+    
+
 }
