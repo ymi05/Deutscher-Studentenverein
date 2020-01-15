13b669250b0f5cc0f7232e7d6fbe3ef4ced9deef
ymi05 <44197563+ymi05@users.noreply.github.com>
2020-01-15 06:34:24 -0800

    Update events.js

--- JS/events.js
+++ JS/events.js
@@ -1,6 +1,6 @@
 loadEvents();
 function loadEvents(){
-    fetch("../JSON_files/events.json")//fetch returns promise
+    fetch("../JSON/events.json")//fetch returns promise
     .then(function(res){
       return res.json(); //returns the text
     })
