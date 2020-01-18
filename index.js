e36a192a76d2e3eb49787337c4e2f49cd49cd699
ymi05 <44197563+ymi05@users.noreply.github.com>
2020-01-18 08:29:33 -0800

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-var options = document.querySelectorAll(".navbar-nav a");
-for(let i=0; i<options.length;i++){
-    options[i].addEventListener("click",()=>{
-       document.querySelector("button").click();
-    });
-}
